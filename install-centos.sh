ae80b73c3f0284ca866d8c08846a5d13c747b570
KimTaeWoong <taewoong.kim@nhn.com>
2020-05-14 09:21:31 +0900

    add install-centos.sh

---
+++ install-centos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# origin repo: https://rapphil.github.io/vim-python-ide/
+
+# clone vim-as-pythone-ide
+git clone https://github.com/twkim/vim-python-ide.git
+
+# install packages
+yum install ctags cscope gcc ncurses-devel tcl-devel python python-devel python3 python3-devel tcl-devel
+yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+yum install ripgrep
+pip3 install --user ropevim
+
+./install.sh
